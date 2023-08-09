0e807b40d08446d1f7812ae7f0ddbc3e32fa89c6
Maruk Alena <maruk.lena27@gmail.com>
2023-08-09 12:50:42 +0300

    feat: code done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function toReadable (number) {
-  
+  const single = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const ty = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+  if (number === 0) {
+    return 'zero';
+  }
+
+  if (number < 20) {
+    return single[number];
+  }
+
+  if (number < 20) {
+    return ty[number];
+  }
+
+  if (number < 100) {
+    return ty[Math.floor(number / 10)] + (number % 10 !== 0 ? ' ' + single[number%10]:'')
+  }
+
+  if (number < 1000) {
+  return single[Math.floor(number / 100)] + ' hundred' + (number % 100 !== 0 ? ' ' + toReadable(number % 100): '')
+  }
 }
